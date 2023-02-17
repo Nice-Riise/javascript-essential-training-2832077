640d66d7944e7b741f88d57ac84ddc6040da2a2a
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-02-17 18:13:41 +0100

    fixed the GWEI calculator ++

--- 04_01/script.js
+++ 04_01/script.js
@@ -6,7 +6,7 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
+  "Every second day Backpack",
   30,
   "grey",
   15,
@@ -16,6 +16,29 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
+const content = `
+<main>
+      <article>
+        <h1>${everydayPack.name}</h1>
+        <ul>
+          <li>Volume: ${everydayPack.volume}</li>
+          <li>Color: ${everydayPack.color}</li>
+          <li>Age: ${everydayPack.backpackAge()}</li>
+          <li>Number of pockets: ${everydayPack.pocketNum}</li>
+          <li>Left strap length: ${everydayPack.strapLength.left}</li>
+          <li>Right strap length: ${everydayPack.strapLength.right}</li>
+          <li>Lid status: ${everydayPack.lidOpen}</li>
+        </ul>
+      </article>
+    </main>
+`;
+
+document.body.innerHTML = content;
+
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+
+let number = 5;
+let subtracted = 5 - 1;
+console.log("The number before" + number + "is" + subtracted + ".");
